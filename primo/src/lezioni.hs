f9d240e80ddf5f970e33557446d1384cc091164f
sflesca <sergio.flesca@unical.it>
2022-10-11 21:21:40 +0200

    ritocchi

--- primo/src/lezioni.hs
+++ primo/src/lezioni.hs
@@ -16,5 +16,8 @@ sumlength1 xs = (sum xs, length xs)
 sumlength2 :: [Int] -> (Int,Int)
 sumlength2 = foldr (\n (x, y) -> (n + x, 1 + y)) (0, 0)
 
+map1 :: (a -> b) -> [a] -> [b]
 map1 f = foldr (\x xs -> f x : xs) []
+
+filter1 :: (a -> Bool) -> [a] -> [a]
 filter1 p = foldr (\x xs -> if p x then x:xs else xs) []
