70b2a37aa63537ea6c7248b4dc2710aaef941599
sflesca <sergio.flesca@unical.it>
2022-10-11 16:07:57 +0200

    vaire

--- primo/src/lezioni.hs
+++ primo/src/lezioni.hs
@@ -9,3 +9,12 @@ safetail1 (x:xs) = xs
 safetail2 :: Eq a1 => [a1] -> [a1]
 safetail2 xs | xs == [] = []
              | otherwise = tail xs
+
+sumlength1 :: [Int] -> (Int,Int)
+sumlength1 xs = (sum xs, length xs)
+
+sumlength2 :: [Int] -> (Int,Int)
+sumlength2 = foldr (\n (x, y) -> (n + x, 1 + y)) (0, 0)
+
+map1 f = foldr (\x xs -> f x : xs) []
+filter1 p = foldr (\x xs -> if p x then x:xs else xs) []
