37f1dd329fbb727c28a1b953cbdb0319182a6f27
sflesca <sergio.flesca@unical.it>
2022-10-14 18:24:50 +0200

    aggiunto secondo algoritmo

--- primo/src/voting1.hs
+++ primo/src/voting1.hs
@@ -7,9 +7,12 @@ data Vote = Red | Blue | Green
 data VotiPresi  = Esito Vote Int
     deriving (Eq, Show)
 
+toVote :: VotiPresi -> Vote
+toVote (Esito v _ ) = v
+
 instance Ord VotiPresi where
     compare (Esito v1 n1) (Esito v2 n2) | n1>n2                 = LT
-                                        | n1 == n2 && v1 > v2   = LT
+                                        | n1 == n2 && v1 < v2   = LT
                                         | otherwise             = GT
 
 
@@ -31,8 +34,36 @@ winner vs = let Esito v n = minimum (computepoll vs)
             in v
 
 
+
+
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (/= [])
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/= x))
+
+looser :: [Vote] -> Vote
+looser vs = let Esito v n = maximum (computepoll vs)
+            in v
+
+rank :: [[Vote]] -> [Vote]
+rank = map toVote . reverse . sort . computepoll . map head
+
+winner1 :: [[Vote]] -> Vote
+winner1 bs =   case rank (rmempty bs) of
+                [c]     -> c
+                (c:cs) -> winner1 (elim c bs)
+                []-> error "non deve succedere"
+
 votes :: [Vote]
-votes = [Red, Blue, Green, Blue, Blue, Red]
+votes = [Red, Blue, Blue, Blue, Red, Red]
 
 votipresitotali :: [VotiPresi]
 votipresitotali = [Esito Red 3, Esito Green 1, Esito Blue 3]
+
+ballots :: [[Vote]]
+ballots = [[Red, Green],
+           [Blue],
+           [Green, Red, Blue],
+           [Blue, Green, Red],
+           [Green]]
