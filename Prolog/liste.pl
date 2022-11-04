572d58dec7d3951a4222ed9fb529bb2812a9ade5
sflesca <sergio.flesca@unical.it>
2022-11-04 10:20:44 +0100

    piccole modifiche

--- Prolog/liste.pl
+++ Prolog/liste.pl
@@ -4,9 +4,17 @@ append([X|L],M,[X|N]) :- append(L,M,N).
 member(X,  [X|_]).
 member(X, [_|T]) :- member(X, T).
 
+remove(_,[],[]).
 remove(X, [X|L], L).
 remove(X, [Y|L], [Y|M]) :- remove(X, L, M).
 
+removefirst(_,[],[]).
+removefirst(X, [X|L], L).
+removefirst(X, [Y|L], [Y|M]) :- X\= Y, removefirst(X, L, M).
+
+removeAll(_,[],[]).
+removeAll(X, [X|L], L1):- removeAll(X ,L, L1).
+removeAll(X, [Y|L], [Y|M]) :- X\= Y, removeAll(X, L, M).
 
 zip([],[],[]).
 zip([X|L], [Y|M], [(X,Y)|N]) :- zip(L, M, N).
