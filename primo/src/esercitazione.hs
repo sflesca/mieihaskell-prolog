c4cb607b482c3b92da1ab33194b132e37536e509
sflesca <sergio.flesca@unical.it>
2022-12-16 22:34:25 +0100

    esehaskell

---
+++ primo/src/esercitazione.hs
@@ -0,0 +1,38 @@
+minOdd :: [Int] -> (Int, Int)
+minOdd [] = (maxBound::Int,maxBound::Int)
+minOdd (x : xs) | mod x 2 == 1 = let (y,z) = minOdd xs in if x<y then (x,y) else if x<z then (y,x) else (y,z)
+                | otherwise    = minOdd xs
+
+partialSums :: Num a => [a] -> [a]
+partialSums (x:xs) = x : map (+ x) (partialSums xs)
+partialSums []     = []
+
+listWithPartialSums :: Num b => [b] -> [(b, b)]
+listWithPartialSums xs = zip xs (partialSums xs)
+
+shiftToZero :: (Num a, Ord a) => [a] -> [a]
+shiftToZero [] = []
+shiftToZero xs = map ((-minimum xs)+ ) xs
+
+
+data  BST a = Void | Node a (BST a ) (BST a ) deriving (Eq, Ord, Read, Show)
+
+somma :: (Num a) => BST a -> a
+somma Void = 0
+somma (Node x sn ds) = x + somma sn + somma ds
+
+samesums :: (Num a, Eq a) => [BST a] -> Bool
+samesums [] = True
+samesums xs = allequals (map somma xs)
+
+allequals :: (Num a, Eq a) => [a] -> Bool
+allequals [] = True
+allequals (x:xs) =  all (==x) xs
+
+
+bstElem :: (Eq t, Ord t) => t -> BST t -> Bool
+bstElem x Void = False
+bstElem x (Node y sn ds) 
+            | x==y       = True
+            | x < y      = bstElem x sn
+            | otherwise  = bstElem x ds
