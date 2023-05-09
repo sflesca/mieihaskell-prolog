009e1b5ff2367f21494598f0be5d387300bb6a02
sflesca <sergio.flesca@unical.it>
2023-05-09 13:14:52 +0200

    Create prove.hs

---
+++ primo/src/prove.hs
@@ -0,0 +1,8 @@
+delete _ [] = []
+delete x lista = takeWhile (\y -> y /= x) lista ++ [x] ++ resto x lista
+
+resto:: Int -> [Int] -> [Int]
+resto _ [] = []
+resto x (y:ys) 
+    | y == x = filter(\z -> z /=x) ys
+    | otherwise = resto x ys
