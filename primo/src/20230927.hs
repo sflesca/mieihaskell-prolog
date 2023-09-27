06fd710e9f1203eb6c5b504f0931c8cfdea15075
sflesca <sergio.flesca@unical.it>
2023-09-27 10:40:15 +0200

    Create 20230927.hs

---
+++ primo/src/20230927.hs
@@ -0,0 +1,17 @@
+myLast1 :: [a] -> a
+myLast1 [] = error "La lista è vuota"
+myLast1 [x] = x
+myLast1 (_:xs) = myLast xs
+
+myLast2 :: [a] -> a
+myLast2 [] = error "La lista è vuota"
+myLast2 xs = xs !! (length xs - 1)
+
+init1 :: [a] -> [a]
+init1 [] = error "La lista è vuota"
+init1 [x] = []
+init1 (x:xs) = x : init1 xs
+
+init2 :: [a] -> [a]
+init2 [] = error "init': empty list"
+init2 xs = reverse (tail (reverse xs))
