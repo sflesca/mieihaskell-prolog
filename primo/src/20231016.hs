33fc5674377be951844b488a3d163ed6d689f1de
sflesca <sergio.flesca@unical.it>
2023-10-16 22:58:48 +0200

    Update 20231016.hs

--- primo/src/20231016.hs
+++ primo/src/20231016.hs
@@ -1,6 +1,19 @@
 
+
+quadratipiccoli :: (Num a, Ord a) => [a] -> [a]
+quadratipiccoli xs = filter (<1000) (map (^2) xs)
+
+
+
+
+
+miaconcat :: [a] -> [a] -> [a]
+miaconcat xs ys = foldr (:) ys xs
+
+
+
 fusion :: (a1 -> Bool) -> (a2 -> a1) -> [a2] -> [a1]
-fusion p f xs = filter p (map f xs)
+fusion p f myxs = filter p (map f myxs)
 
 fusion1 :: (a1 -> Bool) -> (a2 -> a1) -> [a2] -> [a1]
 fusion1 p f = foldr (\x xs -> if p (f x) then f x : xs else xs) []
