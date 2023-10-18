cd1e0720fed40b6ecac422c5d1d3392d36bd54b0
sflesca <sergio.flesca@unical.it>
2023-10-18 17:06:12 +0200

    sss

--- primo/src/esetipi.hs
+++ primo/src/esetipi.hs
@@ -26,6 +26,9 @@ folde f g h (Val n) = f n
 folde f g h (Add e1 e2) = g (folde f g h e1) (folde f g h e2)
 folde f g h (Mul e1 e2) = h (folde f g h e1) (folde f g h e2)
 
+exp1::Expr
+exp1 = Add (Val 10) (Mul (Add (Val 2) (Val 3)) (Val 4))
+
 
 data Tree a = Leaf a | Node a (Tree a) (Tree a)
     deriving Show
