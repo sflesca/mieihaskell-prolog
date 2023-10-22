eb88a52b0731c7b989dd19cf46edf329e0773bd8
sflesca <sergio.flesca@unical.it>
2023-10-22 22:36:02 +0200

    sss

--- primo/src/prove.hs
+++ primo/src/prove.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 delete :: Int -> [Int] -> [Int]
 delete _ [] = []
 delete x lista = takeWhile (\y -> y /= x) lista ++ [x] ++ resto x lista
@@ -7,3 +8,18 @@ resto _ [] = []
 resto x (y:ys) 
     | y == x = filter(\z -> z /=x) ys
     | otherwise = resto x ys
+
+data Maybe1 a = Nothing1 | Just1 a
+
+instance Functor Maybe1 where
+    fmap :: (a -> b) -> Maybe1 a -> Maybe1 b
+    fmap f (Just1 x) = Just1 (f x)
+    fmap f Nothing1 = Nothing1
+
+
+instance Applicative Maybe1 where
+    pure :: a -> Maybe1 a
+    pure = Just1
+    (<*>) :: Maybe1 (a -> b) -> Maybe1 a -> Maybe1 b
+    Nothing1 <*> _ = Nothing1
+    (Just1 f) <*> something = fmap f something
--- primo/src/schifezze.hs
+++ primo/src/schifezze.hs
@@ -1,3 +1,5 @@
-double x = x+x
+data Tree a = Leaf a | Node a (Tree a) (Tree a)
+instance Functor Tree where
+    fmap f (Leaf x) = Leaf (f x)
+    fmap f (Node x t1 t2) = Node (f x) (fmap f t1) (fmap f t2)
 
-quadruple x = double (double x)
