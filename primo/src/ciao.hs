dbc0a7583ac67f061ad347d2e86d340ffe89659b
sflesca <sergio.flesca@unical.it>
2023-12-05 10:16:37 +0100

    xxx

--- primo/src/ciao.hs
+++ primo/src/ciao.hs
@@ -1,10 +1,14 @@
-sommeCoppie :: [Int] -> [(Int, Int)]
-sommeCoppie lista = sommeCoppieAux 0 (sum lista) lista
+coppie :: [Int] -> [(Int, Int)]
+coppie lista = coppieAux 0 (sum lista) lista
   where
-    sommeCoppieAux _ _ [] = []
-    sommeCoppieAux precSum succSum (x:xs) =
+    coppieAux _ _ [] = []
+    coppieAux precSum succSum (x:xs) =
       let nuovoPrecSum = precSum + x
           nuovoSuccSum = succSum - x
-      in (precSum, nuovoSuccSum) : sommeCoppieAux nuovoPrecSum nuovoSuccSum xs
+      in (precSum, nuovoSuccSum) : coppieAux nuovoPrecSum nuovoSuccSum xs
 
 
+scalare :: [Int] -> [Int] -> Int
+scalare [] [] = 0
+scalare (x:xs) (y:ys) = x*y+ scalare xs ys
+scalare xs ys = error "liste di lunghezza diversa: non deve succedere"
